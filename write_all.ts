48c1f1e8feaa42a5561e80f00bd9bae2bee96011
Alisue <lambdalisue@gmail.com>
2023-05-07 13:26:56 +0900

    Allow `options` to regulate piping behavior of `writeAll`

--- write_all.ts
+++ write_all.ts
@@ -1,21 +1,47 @@
-export type WriteAllOptions = {
+export type WriteAllOptions = PipeOptions & {
   /** The size of each chunk to write to the stream */
   chunkSize?: number;
 };
 
 /**
- * Write all data from a Uint8Array to a writable stream.
+ * Writes all data in a Uint8Array to a writable stream in chunk-size units.
  *
- * @param stream The stream to write to.
- * @param data The data to write.
- * @returns A promise that resolves when all the data has been written to the stream.
+ * ```ts
+ * import { assertEquals } from "https://deno.land/std@0.186.0/testing/asserts.ts";
+ * import { writeAll } from "./write_all.ts";
+ *
+ * const encoder = new TextEncoder();
+ * const chunks: Uint8Array[] = [];
+ * const stream = new WritableStream({
+ *   write(chunk) {
+ *     chunks.push(chunk);
+ *   },
+ * });
+ * await writeAll(stream, encoder.encode("HelloWorld"), { chunkSize: 3 });
+ * assertEquals(chunks, [
+ *   encoder.encode("Hel"),
+ *   encoder.encode("loW"),
+ *   encoder.encode("orl"),
+ *   encoder.encode("d"),
+ * ]);
+ * ```
+ *
+ * @param {WritableStream<Uint8Array>} stream The stream to write to.
+ * @param {Uint8Array} data The data to write.
+ * @param {WriteAllOptions} [options={}] Options for configuring the piping behavior.
+ * @param {AbortSignal} [options.signal] AbortSignal to abort ongoing pipe operation.
+ * @param {boolean} [options.preventCancel] Prevent the source from being canceled.
+ * @param {boolean} [options.preventClose] Prevent the source from being closed.
+ * @param {boolean} [options.preventAbort] Prevent the source from being aborted.
+ * @param {number} [options.chunkSize=1024] The size of each chunk to write to the stream.
+ * @returns {Promise<void>} A promise that resolves when all the data has been written to the stream.
  */
 export async function writeAll(
   stream: WritableStream<Uint8Array>,
   data: Uint8Array,
   options: WriteAllOptions = {},
 ): Promise<void> {
-  const { chunkSize = 1024 } = options;
+  const { chunkSize = 1024, ...rest } = options;
   const input = new ReadableStream({
     start(controller) {
       let offset = 0;
@@ -26,5 +52,5 @@ export async function writeAll(
       controller.close();
     },
   });
-  await input.pipeTo(stream);
+  await input.pipeTo(stream, rest);
 }
