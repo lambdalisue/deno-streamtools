4877101adbfce9c397e6ee02558de83731114af6
Alisue <lambdalisue@gmail.com>
2023-05-07 13:26:41 +0900

    Add `options` to regulate piping behavior of `readAll`

--- read_all.ts
+++ read_all.ts
@@ -1,15 +1,37 @@
-import { concat } from "https://deno.land/std@0.185.0/bytes/mod.ts";
+import { concat } from "https://deno.land/std@0.186.0/bytes/mod.ts";
 import { collect } from "./collect.ts";
 
 /**
  * Reads all available bytes from a given `ReadableStream<Uint8Array>` and concatenates them into a single `Uint8Array`.
  *
- * @param stream The stream to read from.
- * @returns A promise that resolves to a `Uint8Array` containing all the bytes read from the stream.
+ * ```ts
+ * import { assertEquals } from "https://deno.land/std@0.186.0/testing/asserts.ts";
+ * import { readAll } from "./read_all.ts";
+ *
+ * const encoder = new TextEncoder();
+ * const stream = new ReadableStream({
+ *   start(controller) {
+ *     controller.enqueue(encoder.encode("Hello"));
+ *     controller.enqueue(encoder.encode("World"));
+ *     controller.close();
+ *   },
+ * });
+ * const result = await readAll(stream);
+ * assertEquals(result, encoder.encode("HelloWorld"));
+ * ```
+ *
+ * @param {ReadableStream<Uint8Array>} stream The stream to read from.
+ * @param {PipeOptions} [options={}] Options for configuring the piping behavior.
+ * @param {AbortSignal} [options.signal] AbortSignal to abort ongoing pipe operation.
+ * @param {boolean} [options.preventCancel] Prevent the source from being canceled.
+ * @param {boolean} [options.preventClose] Prevent the source from being closed.
+ * @param {boolean} [options.preventAbort] Prevent the source from being aborted.
+ * @returns {Promise<Uint8Array>} A promise that resolves to a `Uint8Array` containing all the bytes read from the stream.
  */
 export async function readAll(
   stream: ReadableStream<Uint8Array>,
+  options: PipeOptions = {},
 ): Promise<Uint8Array> {
-  const chunks = await collect(stream);
+  const chunks = await collect(stream, options);
   return concat(...chunks);
 }
